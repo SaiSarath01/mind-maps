490e4e62de440e4de450f80957796a48ec462813
Sai_Sarath0101 <sarath@prajna-labs.com>
2021-07-13 22:47:39 +0530

    initial push

---
+++ config/database.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+
+module.exports = function connect(connectionString) {
+  mongoose.connect(connectionString,{useNewUrlParser: true});
+  var db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error"));
+  db.once("open", function () {
+    console.log("Mongoose connected at: ", connectionString);
+  });
+};
